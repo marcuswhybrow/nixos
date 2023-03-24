46c15e8485b03f64d3bdd0923d9c854020d0aff6
Marcus Whybrow <marcus@whybrow.uk>
2023-03-24 23:41:43 +0000

    Remove lightdm Display Manager

--- configuration.nix
+++ configuration.nix
@@ -38,7 +38,7 @@
     # services.xserver.displayManager.gdm.enable = true;
     # services.xserver.desktopManager.gnome.enable = true;
 
-    services.xserver.displayManager.lightdm.enable = true;
+    # services.xserver.displayManager.lightdm.enable = true;
 
     programs.fish.enable = true;
 
