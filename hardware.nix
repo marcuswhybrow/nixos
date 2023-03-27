117fb91c97ba5a02f60f410766d94f300df6c904
Marcus Whybrow <marcus@whybrow.uk>
2023-03-27 15:50:34 +0100

    Enable Intel SGX on Intel CPUs

--- hardware.nix
+++ hardware.nix
@@ -2,12 +2,15 @@
   inherit (lib) mkDefault mkOption types;
   cfg = config.custom.hardware;
 in {
-  options.custom.hardware.cpu = mkOption {
-    type = types.enum [ "intel" "amd" ];
+  options.custom.hardware = {
+    cpu = mkOption { type = types.enum [ "intel" "amd" ]; };
   };
 
   config = {
     hardware.enableRedistributableFirmware = mkDefault true;
-    hardware.cpu.${cfg.cpu}.updateMicrocode = mkDefault true;
+    hardware.cpu.${cfg.cpu} = {
+      updateMicrocode = mkDefault true;
+      sgx.provision.enable = cfg.cpu == "intel";
+    };
   };
 }
