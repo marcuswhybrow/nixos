4872e37c5a4dabdd63f1bb529594ee9bfa4d4fec
Marcus Whybrow <marcus@whybrow.uk>
2023-03-27 09:42:23 +0100

    Add firewall options to networking

--- networking.nix
+++ networking.nix
@@ -4,13 +4,23 @@
 in {
   options.custom.networking = {
     hostName = mkOption { type = types.str; };
+    firewall = {
+      enable = mkOption { type = types.bool; default = true; };
+      open.tcp = mkOption { type = with types; listOf int; default = []; };
+      open.udp = mkOption { type = with types; listOf int; default = []; };
+    };
   };
 
   config = {
     networking = {
       useDHCP = mkDefault true;
       hostName = cfg.hostName;
-      networkmanager.enable = true;
+      networkmanager.enable = mkDefault true;
+      firewall = {
+        enable = mkDefault cfg.firewall.enable;
+        allowedTCPPorts = cfg.firewall.open.tcp;
+        allowedUDPPorts = cfg.firewall.open.udp;
+      };
     };
   };
 }
