2ee8c5ef51f363b6deb199032e8098df40db18f4
Marcus Whybrow <marcus@whybrow.uk>
2023-03-27 16:12:30 +0100

    Enable Accelerated Video Playback on Intel

--- hardware.nix
+++ hardware.nix
@@ -1,6 +1,7 @@
 { config, pkgs, lib, ... }: let
-  inherit (lib) mkDefault mkOption types;
+  inherit (lib) mkDefault mkOption types mkIf;
   cfg = config.custom.hardware;
+  isIntel = cfg.cpu == "intel";
 in {
   options.custom.hardware = {
     cpu = mkOption { type = types.enum [ "intel" "amd" ]; };
@@ -10,7 +11,23 @@ in {
     hardware.enableRedistributableFirmware = mkDefault true;
     hardware.cpu.${cfg.cpu} = {
       updateMicrocode = mkDefault true;
-      sgx.provision.enable = cfg.cpu == "intel";
+      sgx.provision.enable = isIntel; 
+    };
+
+    # https://nixos.wiki/wiki/Accelerated_Video_Playback
+    nixpkgs.config.packageOverrides = pkgs: {
+      vaapiIntel = pkgs.vaapiIntel.override {
+        enableHybridCodec = true;
+      };
+    };
+    hardware.opengl = mkIf isIntel {
+      enable = true;
+      extraPackages = with pkgs; [
+        intel-media-driver
+        vaapiIntel
+        vaapiVdpau
+        libvdpau-va-gl
+      ];
     };
   };
 }
