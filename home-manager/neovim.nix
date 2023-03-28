d4cad9bad367ed7a1af5ae985a4beaac69b76684
Marcus Whybrow <marcus@whybrow.uk>
2023-03-28 17:31:16 +0100

    Use github-nvim-theme light, for neomvim

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -1,6 +1,16 @@
 { config, lib, pkgs, ... }: let
   inherit (lib) mkEnableOption mkOption types mkIf;
   inherit (builtins) mapAttrs;
+  github-nvim-theme = pkgs.vimUtils.buildVimPluginFrom2Nix rec {
+    pname = "github-nvim-theme";
+    version = "0.0.7";
+    src = pkgs.fetchFromGitHub {
+      owner = "projekt0n";
+      repo = "github-nvim-theme";
+      rev = "refs/tags/v${version}";
+      sha256 = "sha256-Qm9ffdkHfG5+PLQ8PbOeFMywBbKVGqX8886clQbJzyg=";
+    };
+  };
 in {
   options.custom.users = mkOption { type = with types; attrsOf (submodule {
     options.neovim = {
@@ -23,8 +33,9 @@ in {
           vim-nix
           gruvbox
           catppuccin-nvim # https://github.com/catppuccin/nvim
+          github-nvim-theme
         ];
-        extraConfig = ''colorscheme catppuccin-latte'';
+        extraConfig = ''colorscheme github_light'';
       };
     }) config.custom.users;
   };
