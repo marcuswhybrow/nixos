6c1df701a70d4d8a6ca24347a8af84b8380f6028
Marcus Whybrow <marcus@whybrow.uk>
2023-03-29 16:20:18 +0100

    Add light rofi theme

--- themes/light.nix
+++ themes/light.nix
@@ -4,9 +4,6 @@
   inherit (lib.attrsets) mapAttrsToList zipAttrs;
   inherit (import ../utils { inherit lib; }) mapAttrsToListAndMerge merge;
 in {
-  programs.rofi = {
-    font = "Droid Sans Mono 14";
-  };
 
   programs.alacritty.settings.colors = merge [
     {
@@ -94,5 +91,80 @@ in {
     #workspaces button:hover   { color: black; }
     #workspaces button.focused { color: black; }
     #workspaces button.urgent  { color: #C9545D; }
+  '';
+
+  # Modified from https://github.com/anstellaire/photon-rofi-themes
+  programs.rofi = {
+    font = "Droid Sans Mono 10";
+    theme = "mw-light";
+  };
+  xdg.dataFile."rofi/themes/mw-light.rasi".text = ''
+    * {
+        bg:           #ffffff;
+        bg-border:    #000000;
+        fg:           #333333;
+        accent-bg:    #330000;
+        accent-fg:    #ffffff;
+
+        spacing:          10;
+
+        background-color: @bg;
+    }
+
+    window {
+        width:            40%;
+        border:           2;
+        padding:          5;
+        background-color: @bg;
+        border-color:     @bg-border;
+    }
+
+    inputbar {
+        spacing:    0px;
+        padding:    5px 5px 0px 5px;
+        text-color: @fg;
+        children:   [ prompt,textbox-prompt-colon,entry,case-indicator ];
+    }
+
+    prompt {
+        spacing:    0;
+        text-color: @fg;
+    }
+
+    textbox-prompt-colon {
+        expand:     false;
+        str:        ":";
+        margin:     0px 0.3em 0em 0em ;
+        text-color: @fg;
+    }
+
+    entry {
+        spacing:    5px;
+        text-color: @fg;
+    }
+
+    listview {
+        fixed-height: 0;
+        spacing:      0px;
+        scrollbar:    false;
+        lines:        20;
+    }
+
+    element {
+        border:  0;
+        padding: 5px 5px 5px 5px;
+        background-color: @bg;
+        text-color:       @fg;
+    }
+
+    element.selected {
+        background-color: @accent-bg;
+        text-color:       @accent-fg;
+    }
+
+    element-text {
+        background-color: inherit;
+        text-color:       inherit;
+    }
   '';
 }
