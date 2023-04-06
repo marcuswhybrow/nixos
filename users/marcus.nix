8113ba3a1375a0f64151c02991d4a7520ac5f9b1
Marcus Whybrow <marcus@whybrow.uk>
2023-04-06 22:38:29 +0100

    Refactor NeoVim theme into users/marcus.nix

--- users/marcus.nix
+++ users/marcus.nix
@@ -110,17 +110,29 @@
         # Docs for install Vim/NeoVim packages with Nix
         # https://github.com/NixOS/nixpkgs/blob/b740337fb5b41e893d456e3b6cd5b62b6dad5098/doc/languages-frameworks/vim.section.md
 
-        # https://github.com/xiyaowong/transparent.nvim
-        (pkgs.vimUtils.buildVimPluginFrom2Nix {
-          name = "transparent.nvim";
+        # https://github.com/projekt0n/github-nvim-theme
+        (pkgs.vimUtils.buildVimPluginFrom2Nix rec {
+          pname = "github-nvim-theme";
+          version = "0.0.7";
           src = pkgs.fetchFromGitHub {
-            owner = "xiyaowong";
-            repo = "transparent.nvim";
-            rev = "2d8d650fc8a3b8da6a01031295547295eb473b7d";
-            hash = "sha256-6NmVgXlA7Afwwf+XEXJRAp1LIvaFTEQbxg1u1Hqoq1A=";
+            owner = "projekt0n";
+            repo = "github-nvim-theme";
+            rev = "refs/tags/v${version}";
+            sha256 = "sha256-Qm9ffdkHfG5+PLQ8PbOeFMywBbKVGqX8886clQbJzyg=";
           };
         })
       ];
+
+      extraLuaConfig = ''
+        require("github-theme").setup({
+          transparent = true,
+          theme_style = "light",
+          colors = {
+            hint = "orange",
+            error = "#ff0000"
+          }
+        })
+      '';
     };
     
     programs.rofi.enable = true;
