0d61d04564fb8c408a0ec8f056d631d8327473d1
Marcus Whybrow <marcus@whybrow.uk>
2023-04-07 16:46:37 +0100

    Add double quote (") to end of fish "git commit" abbr

--- users/marcus/fish.nix
+++ users/marcus/fish.nix
@@ -15,7 +15,7 @@
 
         gs = ''git status'';
         ga = ''git add .'';
-        gc = ''git commit -am'';
+        gc = ''git commit -am "'';
         gp = ''git push'';
       };
       functions = {
