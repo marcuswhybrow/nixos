1e3eeb0a1ca114a9bc0393641c974b8cc20d73a7
Marcus Whybrow <marcus@whybrow.uk>
2023-04-07 16:45:28 +0100

    Set gh protocol to SSH

--- users/marcus/git.nix
+++ users/marcus/git.nix
@@ -1,6 +1,9 @@
 { config, pkgs, lib, ... }: {
   config.home-manager.users.marcus = {
-    programs.gh.enable = true;
+    programs.gh = {
+      enable = true;
+      settings.git_protocol = "ssh";
+    };
     programs.git = {
       enable = true;
       userName = "Marcus Whybrow";
